be02b4a812784cde421e70960a67e441d3e0a717
angelgregory <aglansangan@gmail.com>
2023-10-11 01:16:36 +0800

    fixing styles

--- src/components/IFrame.jsx
+++ src/components/IFrame.jsx
@@ -19,8 +19,7 @@ const IFrame = ({ children }) => {
          var link = iframeDocument.createElement("link");
          link.setAttribute("rel", "stylesheet");
          link.setAttribute("type", "text/css");
-         link.setAttribute("href", "/components/index.css");
-
+         link.setAttribute("href", "src/index.css");
          otherhead.appendChild(link);
       }
    }, [ref]);
