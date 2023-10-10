ac9beb7400a8037d5426b0136a6ebca4d2132968
angelgregory <aglansangan@gmail.com>
2023-10-10 22:50:31 +0800

    debugging style

--- src/components/ToFrame.jsx
+++ src/components/ToFrame.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 
 const ToFrame = () => {
    useEffect(() => {
-      console.log(document); // <== here I get the document of the iframe parent
+      console.log("your document", document); // <== here I get the document of the iframe parent
       // if I want to access the iframe document
       console.log(window.frames[0].document); // <== this works well
    }, []);
