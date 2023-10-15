e8d04eae7ac1e6cdf0da46db6028f5f8df225701
angelgregory <aglansangan@gmail.com>
2023-10-16 00:05:58 +0800

    trying responsive design

--- src/components/MyWork.jsx
+++ src/components/MyWork.jsx
@@ -8,18 +8,30 @@ import bg from "../assets/images/desktop-bg.png";
 import off from "../assets/images/turn-off.png";
 import WorkFolder from "./WorkFolder";
 import IFrame from "./IFrame";
+
 const MyWork = ({ isClicked, setIsClicked }) => {
    const [isWork, setIsWork] = useState(false);
    return (
       <div>
+         <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+         />
          <style>{iframe}</style>
-         <div className="flex justify-center items-center h-screen">
-            <div className="relative w-[70%]">
-               <img className="w-full aspect-video" src={bg} alt="" />
+         <div
+            className="flex justify-center items-center h-screen"
+            onClick={() => {
+               setIsWork(!isWork), console.log(isWork);
+            }}
+         >
+            <div className="relative w-[70%] h-[70%]">
+               <img className="w-full h-full" src={bg} alt="" />
                <div className="absolute top-0 h-[90%] w-full">
-                  <div className="p-10 h-full">
+                  <div
+                     className={`flex justify-around lg:${"block p-10 h-full"}`}
+                  >
                      <div
-                        className="w-[10%] cursor-pointer pb-5"
+                        className=" w-[30%] lg:w-[10%] cursor-pointer pb-5"
                         onClick={() => {
                            setIsWork(!isWork), console.log(isWork);
                         }}
@@ -37,7 +49,7 @@ const MyWork = ({ isClicked, setIsClicked }) => {
                         />
                         <p className="text-center font-nav">My Portfolio</p>
                      </div>
-                     <div className="w-[10%] cursor-pointer">
+                     <div className="w-[30%] lg:w-[10%] cursor-pointer">
                         <img
                            src={mail}
                            onPointerEnter={(e) =>
@@ -46,7 +58,7 @@ const MyWork = ({ isClicked, setIsClicked }) => {
                            onPointerLeave={(e) => (e.currentTarget.src = mail)}
                            alt="Email Me"
                         />
-                        <p className="text-center font-nav">Email Me</p>
+                        <p className="text-center font-nav">Contact Me</p>
                      </div>
                   </div>
                </div>
@@ -54,14 +66,16 @@ const MyWork = ({ isClicked, setIsClicked }) => {
                   <img
                      className="absolute left-10 cursor-pointer top-[-30] scale-75 hover:scale-100"
                      src={off}
-                     onPointerDown={() => {
+                     onClick={() => {
                         setIsClicked(true);
                      }}
                      alt=""
                   />
                </div>
                {isWork ? (
-                  <div className="w-[80%] h-[80%] absolute top-5 right-5 rounded-md overflow-clip">
+                  <div
+                     className={`w-[80%] h-[80%] absolute top-5 right-5 rounded-md overflow-clip`}
+                  >
                      <IFrame>
                         <WorkFolder />
                      </IFrame>
