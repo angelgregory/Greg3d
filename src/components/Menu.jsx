2bcd493a33303bc8b9c1fa6457dd96ac5b9c3cf9
Angel Gregory Lansangan <73001425+angelgregory@users.noreply.github.com>
2023-11-14 19:25:54 +0800

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 
-const Menu = (open, setIsOpen) => {
+const Menu = ({ setIsOnMenu }) => {
    const [isVisible, setIsVisible] = useState(true);
 
    const handleButtonClick = () => {
       setIsVisible(false);
+      setIsOnMenu(true);
    };
 
    return (
       <div>
          {isVisible && (
             <div className="absolute top-0 flex flex-col gap-5 text-lg justify-center items-center h-screen w-screen bg-black bg-opacity-30 backdrop-blur-sm">
-               <h1 className="font-nav text-white text-5xl drop-shadow-[0_1px_1px_rgba(0,0,0,0.25)]">
+               <h1 className="font-nav text-white text-5xl drop-shadow-[0_1px_1px_rgba(0,0,0,0.25)] portrait:text-center">
                   GREG'S INTERACTIVE PORTFOLIO
                </h1>
                <p className="text-white">try to explore</p>
@@ -19,10 +20,10 @@ const Menu = (open, setIsOpen) => {
                   <a
                      onClick={handleButtonClick}
                      href="#_"
-                     class="relative inline-flex items-center justify-center px-10 py-4 overflow-hidden tracking-tighter text-white bg-gray-800 rounded-lg group hover:text-black"
+                     className="relative inline-flex items-center justify-center px-10 py-4 overflow-hidden tracking-tighter text-white bg-gray-800 rounded-lg group hover:text-black"
                   >
-                     <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-cyan-50 rounded-full group-hover:w-56 group-hover:h-56"></span>
-                     <span class="relative font-bold">Look Around</span>
+                     <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-cyan-50 rounded-full group-hover:w-56 group-hover:h-56"></span>
+                     <span className="relative font-bold">Look Around</span>
                   </a>
                   {/* <a
                      onClick={handleButtonClick}
