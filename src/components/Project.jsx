58b1f841f1e915d39f896de2b96560877036eeaa
Angel Gregory Lansangan <angelgregorylansangan@Angels-MacBook-Air.local>
2025-01-05 23:28:19 +0800

    Feat: added projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,20 +1,24 @@
 import { Carousel } from "flowbite-react";
+import { generateImageList } from "../utils/ListImage";
+const boticaPlus = import.meta.glob("../assets/BoticaPlus/*.{png,jpg,jpeg,svg}", { eager: true });
+console.log(boticaPlus);
+const ymir = import.meta.glob("../assets/Ymir/*.{png,jpg,jpeg,svg}", { eager: true });
+const pillstop = import.meta.glob("../assets/PillStop/*.{png,jpg,jpeg,svg}", { eager: true });
 
-const images = import.meta.glob("../assets/images/skills/*.{png,jpg,jpeg,svg}", { eager: true });
 export function Project() {
-	const imageList = Object.values(images).map((image, index) => {
-		return (
-			<img
-				key={index}
-				src={image.default}
-				alt={`Image ${index + 1}`}
-				className="h-36 w-h-36 transition ease-in-out duration-700 transform hover:scale-110 cursor-pointer"
-			/>
-		);
-	});
+	const classNames = {
+		horizontal: "aspect-video w-max border-blue-500",
+		vertical: "h-fit border-red-500",
+	};
+	const bpImages = generateImageList(boticaPlus, classNames);
+	const ymirImages = generateImageList(ymir, classNames);
+	const pillstopImages = generateImageList(pillstop, classNames);
+
 	return (
-		<div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-			<Carousel>{imageList}</Carousel>
+		<div className="h-56 sm:h-64 xl:h-80 2xl:h-96 border-blue-500 border-solid border-2">
+			<Carousel>{bpImages}</Carousel>
+			<Carousel>{ymirImages}</Carousel>
+			<Carousel>{pillstopImages}</Carousel>
 		</div>
 	);
 }
---
+++ src/utils/ListImage.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+
+const getImageOrientation = (src, callback) => {
+	const img = new Image();
+	img.src = src;
+	img.onload = () => {
+		callback(img.width > img.height ? "horizontal" : "vertical");
+	};
+};
+
+export const generateImageList = (images, classNames = {}) => {
+	return Object.values(images).map((image, index) => {
+		const [orientation, setOrientation] = useState("");
+
+		useEffect(() => {
+			getImageOrientation(image.default, setOrientation);
+		}, [image]);
+
+		const baseClass = "h-36 w-36 transition ease-in-out duration-700 transform hover:scale-110 cursor-pointer";
+		const orientationClass = orientation === "horizontal" ? classNames.horizontal || "" : classNames.vertical || "";
+
+		return (
+			<img
+				key={index}
+				src={image.default}
+				alt={`Image ${index + 1}`}
+				className={`${baseClass} ${orientationClass}`}
+			/>
+		);
+	});
+};
